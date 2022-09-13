cd67b481a480f23ff629a509bb815b9a64cea473
Fabiano De Paula <fabiano.rpaula@gmail.com>
2022-09-13 15:32:13 -0300

    Atacando

--- Assets/Scripts/Heroi.cs
+++ Assets/Scripts/Heroi.cs
@@ -7,6 +7,8 @@ public class Heroi : MonoBehaviour
 {
     private NavMeshAgent Agente;
     private Vector3 Destino;
+    public GameObject Inimigo;
+    public bool atacando = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,30 @@ public class Heroi : MonoBehaviour
     void Update()
     {
         Mover();
+        Atacar();
+    }
+
+    void Atacar()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousepoint = Input.mousePosition;
+            Ray castpoint = Camera.main.ScreenPointToRay(mousepoint);
+            RaycastHit hit;
+            if (Physics.Raycast(castpoint, out hit, Mathf.Infinity))
+            {
+                if(hit.collider.gameObject.tag == "Inimigo")
+                {
+                    Inimigo = hit.collider.gameObject;
+                }
+            }
+        }
+        if(Inimigo != null)
+        {
+            Destino = Inimigo.transform.position;
+            Agente.SetDestination(Destino);
+        }
+        
     }
 
     void Mover()
